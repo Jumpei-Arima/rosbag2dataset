ddcae7316b4e24eea25fba6aaa26901fb58d027d
arima <arijun0307@gmail.com>
2020-12-05 14:06:02 +0900

    update dockerfile

--- docker/run_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-docker run -it --rm \
-    --privileged \
-    -v ${PWD}/../:/root/ \
-    -v ${RWRC_DATASET_DIR}:/root/dataset \
-    --name rwrc20_rosbag2dataset \
-    arima/rosbag2dataset \
-    bash -c "python3 rosbag2dataset.py"
