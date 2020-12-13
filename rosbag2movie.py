78283c358162a03c2001ca658c73cacd45829042
arima <arijun0307@gmail.com>
2020-12-13 14:27:22 +0900

    update for large rosbag

--- rosbag2movie.py
+++ rosbag2movie.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 import os
+import shutil
 import argparse
 
-import numpy as np
 import cv2
-import rosbag
 
 from rosbaghandler import RosbagHandler
 from utils import *
@@ -14,25 +13,45 @@ def main():
     parser.add_argument('--bagfile', type=str, default='bagfiles/hoge.bag')
     parser.add_argument('--image-topic', type=str, default='/usb_camera/image_raw')
     parser.add_argument('--output-dir', type=str, default='bagfiles/movie')
+    parser.add_argument('--frame-rate', type=float, default=30)
     args = parser.parse_args()
 
     rosbag_handler = RosbagHandler(args.bagfile)
-    data = rosbag_handler.read_messages(topics=[args.image_topic])
-    img_data = data[args.image_topic]
-    topic_type = rosbag_handler.get_topic_type(args.image_topic)
-    images = [];
-    if topic_type == "sensor_msgs/CompressedImage":
-        images = convert_CompressedImage(img_data)
-    elif topic_type == "sensor_msgs/Image":
-        images = convert_Image(img_data)
 
-    h,w,c = images[0].shape
+    t0 = rosbag_handler.start_time
+    dt = 100
+    os.makedirs("/tmp/rosbag2dataset", exist_ok=True)
+
+    idx = 0
+    while t0<rosbag_handler.end_time:
+        t1 = t0 + dt
+        if t1>rosbag_handler.end_time:
+            t1 = rosbag_handler.end_time
+
+        data = rosbag_handler.read_messages(topics=[args.image_topic], 
+                start_time=t0, end_time=t1)
+        img_data = data[args.image_topic]
+        topic_type = rosbag_handler.get_topic_type(args.image_topic)
+        if topic_type == "sensor_msgs/CompressedImage":
+            images = convert_CompressedImage(img_data)
+        elif topic_type == "sensor_msgs/Image":
+            images = convert_Image(img_data)
+        for img in images:
+            cv2.imwrite(os.path.join("/tmp/rosbag2dataset", str(idx)+".png"), img)
+            idx+=1
+        t0 = t1
+
+    img = cv2.imread("/tmp/rosbag2dataset/0.png")
+    h,w,c = img.shape
     file_name = os.path.splitext(os.path.basename(args.bagfile))[0]
-    output_path = os.path.join(args.output_dir, file_name+".mp4")
-    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc('m','p','4','v'), 30, (w,h))
-    for i in range(len(images)):
-        out.write(images[i])
+    output_path = os.path.join(args.output_dir, file_name+"-"+args.image_topic.replace('/','_')+".mp4")
+    print("output_path: ", output_path)
+    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc('m','p','4','v'), args.frame_rate, (w,h))
+    for idx in range(len(os.listdir("/tmp/rosbag2dataset"))):
+        img = cv2.imread(os.path.join("/tmp/rosbag2dataset", str(idx)+".png"))
+        out.write(img)
     out.release()
+    shutil.rmtree("/tmp/rosbag2dataset")
 
 if __name__=="__main__":
     main()
